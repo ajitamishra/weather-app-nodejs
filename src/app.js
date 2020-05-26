57ba82557bfd1e3eabfd6f4f3ffc2c6e6661acb4
ajitamishra <ajita00054@gmail.com>
2020-05-26 17:25:22 +0530

    Setup for heroku

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const hbs = require("hbs");
 const app = express();
+const port = process.env.PORT || 3000;
 const geoCode = require("./utils/geocode");
 const forecast = require("./utils/forecast");
 
@@ -84,6 +85,6 @@ app.get("*", (req, res) => {
     errorMessage: "Page not found!",
   });
 });
-app.listen(3000, () => {
-  console.log("Server is up and runnning on port 3000");
+app.listen(port, () => {
+  console.log("Server is up and runnning on port " + port);
 });
